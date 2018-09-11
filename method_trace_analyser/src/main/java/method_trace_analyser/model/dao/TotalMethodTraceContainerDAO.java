86cdb580cfaee581fd8384eb7a706a4b6b258c99
Adityalokesh003 <adityalokesh003@gmail.com>
2018-09-11 13:10:38 +0530

    Dao layer created
    Signed-off-by: Adityalokesh003 <adityalokesh003@gmail.com>

---
+++ method_trace_analyser/src/main/java/method_trace_analyser/model/dao/TotalMethodTraceContainerDAO.java
@@ -0,0 +1,18 @@
+package method_trace_analyser.model.dao;
+
+import java.io.File;
+
+import method_trace_analyser.model.bo.TotalMethodTraceContainer;
+
+public interface TotalMethodTraceContainerDAO {
+	
+	public void convertBinaryTraceFileToLog(File traceTRCFile);
+	public TotalMethodTraceContainer getTraceDataFromFile(File traceLogFile);
+	public TotalMethodTraceContainer deleteTraceTRCFile(String trcTraceFileName);
+	public TotalMethodTraceContainer deleteTraceLogFile(String logtraceFileName);
+	public boolean addToDB(TotalMethodTraceContainer traceContainer);
+	public TotalMethodTraceContainer getTraceDataFromDB(String traceFileName);
+	public TotalMethodTraceContainer deleteTraceFromDB(String traceFileName);
+	
+
+}
