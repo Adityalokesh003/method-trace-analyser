d476ed0c2149d8360b5de4c90da4e078cae3f8c6
Adityalokesh003 <adityalokesh003@gmail.com>
2018-09-11 17:45:40 +0530

    DaoImpl is added
    Signed-off-by: Adityalokesh003 <adityalokesh003@gmail.com>

---
+++ method_trace_analyser/src/main/java/method_trace_analyser/model/dao/daoImpl/TotalMethodTraceContainerDaoImpl.java
@@ -0,0 +1,52 @@
+package method_trace_analyser.model.dao.daoImpl;
+
+import java.io.File;
+
+import method_trace_analyser.model.bo.TotalMethodTraceContainer;
+import method_trace_analyser.model.dao.TotalMethodTraceContainerDAO;
+
+public class TotalMethodTraceContainerDaoImpl implements TotalMethodTraceContainerDAO {
+
+	@Override
+	public void convertBinaryTraceFileToLog(File traceTRCFile) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public TotalMethodTraceContainer getTraceDataFromFile(File traceLogFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TotalMethodTraceContainer deleteTraceTRCFile(String trcTraceFileName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TotalMethodTraceContainer deleteTraceLogFile(String logtraceFileName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addToDB(TotalMethodTraceContainer traceContainer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public TotalMethodTraceContainer getTraceDataFromDB(String traceFileName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TotalMethodTraceContainer deleteTraceFromDB(String traceFileName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
