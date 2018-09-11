1f63cbaaf0ad925fe7143e35dd9f06146579778b
Adityalokesh003 <adityalokesh003@gmail.com>
2018-09-11 23:05:29 +0530

    In DaoImpl ,trace file creation and convertion complted
    Signed-off-by: Adityalokesh003 <adityalokesh003@gmail.com>

--- method_trace_analyser/src/main/java/method_trace_analyser/model/dao/TotalMethodTraceContainerDAO.java
+++ method_trace_analyser/src/main/java/method_trace_analyser/model/dao/TotalMethodTraceContainerDAO.java
@@ -5,14 +5,12 @@ import java.io.File;
 import method_trace_analyser.model.bo.TotalMethodTraceContainer;
 
 public interface TotalMethodTraceContainerDAO {
-	
-	public void convertBinaryTraceFileToLog(File traceTRCFile);
+	public boolean createTraceFile(String traceCommand);
+	public boolean convertBinaryTraceFileToLog(File traceTRCFile);
 	public TotalMethodTraceContainer getTraceDataFromFile(File traceLogFile);
 	public TotalMethodTraceContainer deleteTraceTRCFile(String trcTraceFileName);
 	public TotalMethodTraceContainer deleteTraceLogFile(String logtraceFileName);
 	public boolean addToDB(TotalMethodTraceContainer traceContainer);
 	public TotalMethodTraceContainer getTraceDataFromDB(String traceFileName);
 	public TotalMethodTraceContainer deleteTraceFromDB(String traceFileName);
-	
-
 }
--- method_trace_analyser/src/main/java/method_trace_analyser/model/dao/daoImpl/TotalMethodTraceContainerDaoImpl.java
+++ method_trace_analyser/src/main/java/method_trace_analyser/model/dao/daoImpl/TotalMethodTraceContainerDaoImpl.java
@@ -1,22 +1,63 @@
 package method_trace_analyser.model.dao.daoImpl;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import method_trace_analyser.model.bo.TotalMethodTraceContainer;
 import method_trace_analyser.model.dao.TotalMethodTraceContainerDAO;
 
 public class TotalMethodTraceContainerDaoImpl implements TotalMethodTraceContainerDAO {
-
+	
 	@Override
-	public void convertBinaryTraceFileToLog(File traceTRCFile) {
-		// TODO Auto-generated method stub
-		//somthing
-
+	public boolean createTraceFile(String traceCommand) {
+		List<String> command=new ArrayList<String>();
+		command.add("CMD");
+		command.add("/C");
+		command.add("java "+traceCommand);
+		ProcessBuilder processBuilder = new ProcessBuilder(command);
+		processBuilder.directory(new File("path/to/jar_or_class_files"));
+		try {
+			processBuilder.start();
+			//check whether the .trc file created or not
+			//if yes return true
+			//else false
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			//close file
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean convertBinaryTraceFileToLog(File traceTRCFile) {
+		List<String> command=new ArrayList<String>();
+		command.add("CMD");
+		command.add("/C");
+		command.add("java com.ibm.jvm.format.TraceFormat "+traceTRCFile.getName()+".trc "+traceTRCFile.getName()+".log");
+		ProcessBuilder processBuilder = new ProcessBuilder(command);
+		processBuilder.directory(new File("path/to/trace file"));
+		try {
+			processBuilder.start();
+			//check whether the .log file created or not
+			//if yes return true
+			//else false
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			//close file
+		}
+		return false;
 	}
 
 	@Override
 	public TotalMethodTraceContainer getTraceDataFromFile(File traceLogFile) {
-		// TODO Auto-generated method stub
+		// get the trace information from trace files
+		//help: read from the log file line by line
 		return null;
 	}
 
@@ -34,7 +75,7 @@ public class TotalMethodTraceContainerDaoImpl implements TotalMethodTraceContain
 
 	@Override
 	public boolean addToDB(TotalMethodTraceContainer traceContainer) {
-		// TODO Auto-generated method stub
+		// use sqlite
 		return false;
 	}
 
@@ -49,5 +90,7 @@ public class TotalMethodTraceContainerDaoImpl implements TotalMethodTraceContain
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 
 }
