d28146396a61d182e74f43255eea847609fd8dc8
Adityalokesh003 <adityalokesh003@gmail.com>
2018-09-12 21:24:20 +0530

    terminalfx and junit dependencies are added

--- method_trace_analyser/src/main/java/method_trace_analyser/Main.java
+++ method_trace_analyser/src/main/java/method_trace_analyser/Main.java
@@ -1,9 +1,13 @@
 package method_trace_analyser;
 
+import com.kodedu.terminalfx.TerminalBuilder;
+import com.kodedu.terminalfx.TerminalTab;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +16,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/method_trace_analyser/view/Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,600,600);
 			scene.getStylesheets().add(getClass().getResource("/css/main.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -21,6 +25,16 @@ public class Main extends Application {
 			//e.getMessage();
 			 
 		}
+		
+		/*TerminalBuilder terminalBuilder = new TerminalBuilder();
+		TerminalTab terminal = terminalBuilder.newTerminal();
+
+		TabPane tabPane = new TabPane();
+		tabPane.getTabs().add(terminal);
+		Scene scene = new Scene(tabPane,400,400);
+		//scene.getStylesheets().add(getClass().getResource("/css/main.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();*/
 	}
 
 	public static void main(String[] args) {
