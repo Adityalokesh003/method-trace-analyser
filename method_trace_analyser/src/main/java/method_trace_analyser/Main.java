523c3dec871061fb72f05edb9dafa7f0a4885403
Adityalokesh003 <adityalokesh003@gmail.com>
2018-09-18 01:45:52 +0530

    some pom.xml chnages

--- method_trace_analyser/src/main/java/method_trace_analyser/Main.java
+++ method_trace_analyser/src/main/java/method_trace_analyser/Main.java
@@ -1,7 +1,5 @@
 package method_trace_analyser;
 
-import com.kodedu.terminalfx.TerminalBuilder;
-import com.kodedu.terminalfx.TerminalTab;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +13,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/method_trace_analyser/view/Main.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/FXML/Main.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("/css/main.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -25,16 +23,7 @@ public class Main extends Application {
 			//e.getMessage();
 			 
 		}
-		
-		/*TerminalBuilder terminalBuilder = new TerminalBuilder();
-		TerminalTab terminal = terminalBuilder.newTerminal();
-
-		TabPane tabPane = new TabPane();
-		tabPane.getTabs().add(terminal);
-		Scene scene = new Scene(tabPane,400,400);
-		//scene.getStylesheets().add(getClass().getResource("/css/main.css").toExternalForm());
-		primaryStage.setScene(scene);
-		primaryStage.show();*/
+	  
 	}
 	public static void main(String[] args) {
 		launch(args);
