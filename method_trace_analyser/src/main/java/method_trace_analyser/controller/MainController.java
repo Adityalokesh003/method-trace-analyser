754d0afd43125bc65d5ec74b80356bed0111dcde
Adityalokesh003 <adityalokesh003@gmail.com>
2018-09-18 20:38:34 +0530

    controller structure

--- method_trace_analyser/src/main/java/method_trace_analyser/controller/MainController.java
+++ method_trace_analyser/src/main/java/method_trace_analyser/controller/MainController.java
@@ -1,7 +1,15 @@
 package method_trace_analyser.controller;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 
-public class MainController {
+public class MainController implements EventHandler {
+
+	@Override
+	public void handle(Event arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 //this is somthing
 }
--- method_trace_analyser/src/main/java/method_trace_analyser/view/MainApplication.java
+++ method_trace_analyser/src/main/java/method_trace_analyser/view/MainApplication.java
@@ -1,26 +1,28 @@
-package method_trace_analyser;
+package method_trace_analyser.view;
 
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
+public class MainApplication extends Application {
+	 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/FXML/Main.fxml"));
 			Scene scene = new Scene(root);
+			System.out.println("yey");
 			scene.getStylesheets().add(getClass().getResource("/css/main.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
-			//e.getMessage();
+			e.getMessage();
 			 
 		}
 	  
