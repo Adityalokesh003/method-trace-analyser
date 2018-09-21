cd789f956c9685933dfe2565f4da03d9b2c92c62
Adityalokesh003 <adityalokesh003@gmail.com>
2018-09-21 22:12:21 +0530

    random commit before TableView data gathering
    Signed-off-by: Adityalokesh003 <adityalokesh003@gmail.com>

--- method_trace_analyser/src/main/java/method_trace_analyser/controller/MainController.java
+++ method_trace_analyser/src/main/java/method_trace_analyser/controller/MainController.java
@@ -9,17 +9,32 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.layout.AnchorPane;
 
 public class MainController implements Initializable{
 	@FXML
 	private MenuItem open_folder;
 	@FXML
 	private TreeView<String> traceExplorer;
+	@FXML
+	private AnchorPane barchartAnchor;
+	private BarChart<String, Number> graphicalBarChart;
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		traceExplorer.setRoot(LeftTreeViewController.initTreeView());
+		try {
+			//this barchart is not going to be initialized here
+			graphicalBarChart = TabPaneController.getBarChart("fgb");
+			barchartAnchor.getChildren().add(graphicalBarChart);
+			graphicalBarChart.prefWidthProperty().bind(barchartAnchor.widthProperty());
+			graphicalBarChart.prefHeightProperty().bind(barchartAnchor.heightProperty());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public void handleMenuOpenFolder(ActionEvent actionEvent) {
@@ -32,4 +47,5 @@ public class MainController implements Initializable{
 	public void handleUploadTraceFiles(ActionEvent actionEvent) {
 		File traceTRCfile = RightVBoxController.getTRCFromFileChooser();
 	}
+	
 }
